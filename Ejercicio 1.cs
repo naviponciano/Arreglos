eeb6dfa70c471190714a7becb4864bd152312073
naviponciano <navieska.ponciano23906675@estu.edu.ni>
2023-11-05 23:25:15 -0600

    Ejercicio 1

---
+++ Ejercicio 1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ejercicio 1: Alimentar un arreglo de manera estática
+        int[] arregloEstatico = GenerarArregloEstatico(5);
+        
+        // Imprimir el arreglo
+        Console.WriteLine("Ejercicio 1: Arreglo Estático: " + string.Join(", ", arregloEstatico));
+    }
+
+    static int[] GenerarArregloEstatico(int tamaño)
+    {
+        int[] arreglo = new int[tamaño];
+        for (int i = 0; i < tamaño; i++)
+        {
+            arreglo[i] = i + 1;
+        }
+        return arreglo;
+    }
+}
